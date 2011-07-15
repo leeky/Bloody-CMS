29bc114d3d832b873f8a9c6a2b517d64cb702877
Cristiano Betta <cbetta@gmail.com>
2011-07-15 15:35:05 +0100

    Moved twitter details to config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  provider :twitter, CONFIG['twitter']['consumer_key'], CONFIG['twitter']['consumer_secret']
 end
